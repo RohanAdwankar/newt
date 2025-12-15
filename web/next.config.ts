a6ee7f388aee35b07926551fa0e32ea090cc9b5e
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2025-12-14 20:28:16 -0800

    local/remote detection

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'newt';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  /* config options here */
+  basePath: isProd ? `/${repoName}` : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
